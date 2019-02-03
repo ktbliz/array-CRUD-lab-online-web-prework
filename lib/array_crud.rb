f642aceff09e4c9bd2c95b2d468a0ed35c8cc85a
Katie Blizzard <kblizzard11@gmail.com>
2019-02-03 22:52:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  super_bowl = ["rams", "pats", "pizza", "wings"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array <<
 end
 
 def add_element_to_start_of_array(array, element)
